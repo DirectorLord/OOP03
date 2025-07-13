f0b70e66c2038ee781261dae75237e6e73533b99
DirectorLord <thelord161203@gmail.com>
2025-07-13 10:23:45 +0300

    solved rectangle functions

--- OOP03/Program.cs
+++ OOP03/Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace OOP03;
 using System;
- internal class Program
+using System.Drawing;
+
+internal class Program
 {
     #region Question1
     //Write a class named Calculator that contains a method named Add. Overload the Add method to
@@ -12,6 +14,33 @@ using System;
         public int Add(int x, int y) => x + y;
         public int Add(int x, int y, int z) => x + y + z;
         public double Add(double x, double y) => x + y;
+    }
+    #endregion
+
+    #region Question2
+    //    Create a class named Rectangle with the following constructors:
+    //●	A parameterless constructor that sets the width and height to 0.
+    //●	A constructor that accepts width and height as integers.
+    //●	A constructor that accepts a single integer and sets both width and height to that value.
+    public class  Rectangle 
+    {
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public Rectangle()
+        {
+            Width = 0;
+            Height = 0;
+        }
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+        public Rectangle(int size)
+        {
+            Width = size;
+            Height = size;
+        }
     }
     #endregion
     public static void Main(string[] args)

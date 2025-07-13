402273b020b256d4e23e92fb933a48a491689cfb
DirectorLord <thelord161203@gmail.com>
2025-07-13 10:13:32 +0300

    finished question 1

--- OOP03/Program.cs
+++ OOP03/Program.cs
@@ -2,8 +2,22 @@
 using System;
  internal class Program
 {
+    #region Question1
+    //Write a class named Calculator that contains a method named Add. Overload the Add method to
+    //●	Accept two integers and return their sum.
+    //●	Accept three integers and return their sum.
+    //●	Accept two doubles and return their sum
+    public class  Calculator
+    {
+        public int Add(int x, int y) => x + y;
+        public int Add(int x, int y, int z) => x + y + z;
+        public double Add(double x, double y) => x + y;
+    }
+    #endregion
     public static void Main(string[] args)
     {
-        Console.WriteLine("Testing");
+        #region Question1
+
+        #endregion
     }
 }
